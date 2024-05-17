6d29bd8879e4fb5a781a91e271811041c7f6ede3
yohanesrioirsan <rioirsan8@gmail.com>
2024-05-17 12:25:20 +0700

    under development

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,12 @@
-import React, { useEffect } from "react";
-import AOS from "aos";
-import NavBar from "../components/NavBar/NavBar";
-import Header from "../components/Header/Header";
-import Skills from "../components/Skills/Skills";
-import CallToAction from "../components/CallToAction/CallToAction";
-import Footer from "../components/Footer/Footer";
-import Portfolio from "../components/Portfolio/Portfolio";
-import "../App.css";
-import "../components/Header/Header.css";
-import "../components/Portfolio/Portfolio.css";
-import "../components/Skills/Skills.css";
-import "../components/CallToAction/CallToAction.css";
-import "../components/Footer/Footer.css";
-import "../components/Animation/Animation.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "aos/dist/aos.css";
+import React from "react";
+
 
 function Home() {
-  useEffect(() => {
-    AOS.init({ duration: 2000 });
-  }, []);
   return (
     <>
-      <NavBar />
-      <Header />
-      <Portfolio />
-      <Skills />
-      <div className="animation" data-aos="fade-down">
-        <CallToAction />
-        <Footer />
+      <div className="oops" style={{width: "100vw", height:"100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+        <h1 style={{fontSize:"10em"}}>Oops...</h1>
+        <h1 style={{marginTop: "-1rem", opacity: "70%"}}>I am developing a new visual for this website🥲. Come back later.</h1>
       </div>
     </>
   );
