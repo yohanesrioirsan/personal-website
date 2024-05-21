75454f2cbf21bc1bfc1e1df0524d628bea185582
yohanesrioirsan <rioirsan8@gmail.com>
2024-05-21 21:44:29 +0700

    update: progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import NavBar from "./components/navbar/NavBar";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <>
-        <nav className="sticky top-0 flex justify-center">
-          <NavBar />
-        </nav>
+      <div className="container py-4 sm:px-4">
+        <NavBar />
         <main>
-          <section id="work" className="flex justify-center" style={{ height: "200vh" }}>
-            <h1 className="text-4xl">Hello WORLD</h1>
+          <section id="work" className="flex justify-center">
+            <Home />
           </section>
-          <section id="blogs" className="flex justify-center" style={{ height: "200vh" }}>
+          {/* <section
+            id="blogs"
+            className="flex justify-center"
+            style={{ height: "200vh" }}
+          >
             <h1 className="text-4xl">TIDAK HELLO WORLD</h1>
-          </section>
+          </section> */}
         </main>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
+import animations from "@midudev/tailwind-animations";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "1024px",
+          xl: "1280px",
+        },
+      },
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0.0deg)" },
+        },
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [animations],
+};
